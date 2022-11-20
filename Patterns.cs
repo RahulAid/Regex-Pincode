686562ec2d51d4704f69c1079afa5bd85ae0e080
RahulAid <raccess97@gmail.com>
2022-11-21 00:25:52 +0530

    Validate Pin Code

---
+++ Patterns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Patterns
+{
+
+    public void ValidatePinCode(string pincode)
+    {
+        var regex = new Regex("^[1-9][0-9]{5}$");
+        bool isValid = regex.IsMatch(pincode);
+        if (isValid == true)
+        {
+            Console.WriteLine($"\n{pincode} is  valid");
+        }
+        else
+        {
+            Console.WriteLine($"\n{pincode} is invalid");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Regex_Pincode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Regular Expressions Problems");
+
+            Patterns objPattern = new Patterns();
+            objPattern.ValidatePinCode("400088");
+        }
+    }
+}
