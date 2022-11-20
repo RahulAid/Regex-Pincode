431fe1533c6dabfffeeb2ce331ef13cd3aa659be
RahulAid <raccess97@gmail.com>
2022-11-21 01:03:42 +0530

    Check Space In Between

--- Patterns.cs
+++ Patterns.cs
@@ -6,7 +6,7 @@ public class Patterns
 
     public void ValidatePinCode(string pincode)
     {
-        var regex = new Regex("^[1-9][0-9]{5}$");
+        var regex = new Regex("^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$");
         bool isValid = regex.IsMatch(pincode);
         if (isValid == true)
         {
